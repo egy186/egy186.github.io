f411a0c8c4ea07906d4c46b001ec9b6258c3cd22
egy186 <egy186@yahoo.co.jp>
2014-02-08 00:01:05 +0900

    ColorConverter Version 0.2.1

--- colorconverter/v020b/colorconverter.js
+++ colorconverter/v020b/colorconverter.js
@@ -1,11 +1,11 @@
 /*!
- * Color Converter Version 0.2.1
+ * Color Converter Version 0.2.0
  * github.com/egy186/colorConverter
  *
  * Includes egyColorCONFIG
  * gist.github.com/egy186/7693713
  *
- * Copyright (c) 2013-2014 egy186
+ * Copyright (c) 2013 egy186
  * Released under the MIT License.
  */
 
@@ -270,10 +270,10 @@ function init() {
 
     var formInput = document.getElementById('form-input');
     formInput.addEventListener('change', function (evt) {
-        main(evt.target.id, evt.target.value);
+        main(evt.target.name, evt.target.value);
     }, false);
     formInput.addEventListener('input', function (evt) {
-        main(evt.target.id, evt.target.value);
+        main(evt.target.name, evt.target.value);
     }, false);
 
     var formOutputInputs = document.getElementById('form-output').getElementsByTagName('input');
@@ -296,13 +296,13 @@ function init() {
     }
 
     // set color
-    try{
+    if (locHashCv && JSON.parse(locHashCv)) {
         var config = JSON.parse(locHashCv);
         for (var key in config) {
             colorConfig.set(key, config[key]);
             main('num-r', colorConfig.r);
         }
-    } catch (err) {
+    } else {
         main('text-rgba', 'rgba(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',1)');
         console.log('random color');
     }
@@ -332,16 +332,16 @@ function changeTab(newTab) {
     }
     temp = document.getElementsByClassName(newTab.toLowerCase());
     for (i = 0; i < temp.length; i++) {
-        temp[i].style.display = '';
+        temp[i].style.display = 'block';
     }
     delete temp;
     // nav-tab
-    navTabs.item(tabList.indexOf(currentTab)).parentElement.removeAttribute('class');
-    navTabs.item(newTabIndex).parentElement.setAttribute('class', 'active');
+    navTabs.item(tabList.indexOf(currentTab)).removeAttribute('class');
+    navTabs.item(newTabIndex).setAttribute('class', 'current');
     // return
     currentTab = newTab;
     //history.pushState({ code: clicked }, clicked, '#' + clicked);
-    main('num-r', colorConfig.r); // !finally
+    main('r', colorConfig.r); // !finally
 }
 
 
